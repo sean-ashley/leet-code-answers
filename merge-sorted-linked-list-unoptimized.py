f12ce2c2b73e717da14321a453f750329ae379fd
Sean Ashley <sean.d.ashley@gmail.com>
2020-08-30 00:53:58 -0400

    Update merge-sorted-linked-list-unoptimized.py

--- merge-sorted-linked-list-unoptimized.py
+++ merge-sorted-linked-list-unoptimized.py
@@ -1,3 +1,5 @@
+
+  
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,63 +11,31 @@ class Solution:
         
         # Loop thru both lists at the same time, adding the lower of the two elements to the new linked list, and           only incrementing forward the side thats lower.
         
-        
-        
-        #if the l1 has smaller val add it to the list as our fist element, otherwise add the the 1st val from l2
-        if (l1 == None) !=  (l2== None):
-            if l1:
-                return l1
-            else:
-                return l2
-        elif l1 and l2:
-            if l1.val <= l2.val:
-                new_list = ListNode(l1.val)
-                #increment the linked list  so it doesnt get added twice
-                l1 = l1.next
-            else:
-                new_list = ListNode(l2.val)
-                #increment the linked list so it doesnt get added twice
-                l2 = l2.next
-        else:
-            return 
-        #set dummy head
+        #create empty new list
+        new_list = ListNode()
+        #create dummy head to return at the end
         dummy = new_list
         #loop while there still elements in both lists
         while l1 and l2:
-            
             if l1.val <= l2.val:
-                #set the next element to be a listnode with l1.val
-                new_list.next = ListNode(l1.val)
-                #increment the new_list 
-                new_list = new_list.next
+                #set the next element to be  l1
+                new_list.next = l1
                 #increment l1
                 l1 = l1.next
             else:
                 #set the next element to be a listnode with l2.val
-                new_list.next = ListNode(l2.val)
-                #increment the new_list 
-                new_list = new_list.next
+                new_list.next = l2
                 #increment l2
                 l2 = l2.next
+            #increment the new_list
+            new_list = new_list.next
         #if the lists arnt equal, there are gonna be some unsorted things left. so we need to add those to our new list.
-        if l1:
-            while l1:
-                new_list.next = ListNode(l1.val)
-                #increment the new_list 
-                new_list = new_list.next
-                #increment l1
-                l1 = l1.next
+        new_list.next = l1 or l2
+        return dummy.next
+            
+            
                 
-        elif l2:
-            while l2:
-                new_list.next = ListNode(l2.val)
-                #increment the new_list 
-                new_list = new_list.next
-                #increment l2
-                l2 = l2.next
-        #return the dummy
-        return dummy
-            
-            
+
+
                 
         
