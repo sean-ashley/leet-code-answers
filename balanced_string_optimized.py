1172af6f5cc12118d836b4c0231016781ba233f9
Sean Ashley <sean.d.ashley@gmail.com>
2020-10-02 21:38:55 -0400

    Create balanced_string_optimized.py

---
+++ balanced_string_optimized.py
@@ -0,0 +1,17 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        r_counter = 0
+        l_counter = 0
+        total_count = 0
+        
+        for i in s:
+            if i == 'R':
+                r_counter +=1
+            else:
+                l_counter += 1
+            
+            if r_counter == l_counter:
+                total_count += 1
+                
+            
+        return total_count
