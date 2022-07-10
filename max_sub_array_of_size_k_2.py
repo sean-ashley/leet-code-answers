9a2b329d5d5564b9a11f8a102efcf2588d9f6d73
Sean Ashley <sean.d.ashley@gmail.com>
2022-07-10 00:13:47 -0400

    Create max_sub_array_of_size_k_2.py

---
+++ max_sub_array_of_size_k_2.py
@@ -0,0 +1,12 @@
+def max_sub_array_of_size_k(k, arr):
+  max_sum , window_sum = 0, 0
+  window_start = 0
+
+  for window_end in range(len(arr)):
+    window_sum += arr[window_end]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if window_end >= k-1:
+      max_sum = max(max_sum, window_sum)
+      window_sum -= arr[window_start]  # subtract the element going out
+      window_start += 1  # slide the window ahead
+  return max_sum
